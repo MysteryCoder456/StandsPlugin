001aad3e8ff4ff2db62c7c02d76fd11c9c6e93bc
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-11-06 22:51:14 +0400

    added knockback for Star Platinum

--- src/com/mysterycoder456/standsplugin/listeners/StandUserListener.java
+++ src/com/mysterycoder456/standsplugin/listeners/StandUserListener.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 import com.mysterycoder456.standsplugin.Main;
 import com.mysterycoder456.standsplugin.listeners.utils.Utils;
@@ -25,18 +26,28 @@ public class StandUserListener implements Listener {
 	
 	@EventHandler
 	public void onPlayerInteractEvent(PlayerInteractEntityEvent e) {
-		double totalDamage = 16;
-//		int damageCount = 6;
 		Player player = e.getPlayer();
-		Entity rightClickedEntity = e.getRightClicked();
 		
 		@SuppressWarnings("deprecation")
 		ItemStack itemInPlayerHand = player.getItemInHand();
 		
 //		Star Platinum
 		if (itemInPlayerHand.getType() == Material.IRON_INGOT) {
+			double totalDamage = 16;
+//			int damageCount = 6;
+			double knockbackMultiplier = 2;
+			
+			Entity rightClickedEntity = e.getRightClicked();
+			
+			Vector knockbackVector = player.getLocation().getDirection().normalize();
+			knockbackVector.setX(knockbackVector.getX() * knockbackMultiplier);
+			knockbackVector.setY(knockbackVector.getY() * knockbackMultiplier);
+			knockbackVector.setZ(knockbackVector.getZ() * knockbackMultiplier);
+			
+//			Apply damage and knockback
 			Bukkit.broadcastMessage(Utils.chat("&a" + player.getName() + "'s Star Platinum: &7ORA ORA ORA ORA ORA!"));
 			((Damageable) rightClickedEntity).damage(totalDamage);
+			rightClickedEntity.setVelocity(knockbackVector);
 			
 //			TODO: Add repeated damage
 //			for (int i = 0; i < damageCount; i++) {
