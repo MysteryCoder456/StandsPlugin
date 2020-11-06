62f501797952cca99a39942bb4a7a4451f4c3f68
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-11-07 00:18:58 +0400

    added repeated damage with Star Platinum

--- src/com/mysterycoder456/standsplugin/listeners/StandUserListener.java
+++ src/com/mysterycoder456/standsplugin/listeners/StandUserListener.java
@@ -2,6 +2,7 @@ package com.mysterycoder456.standsplugin.listeners;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -16,11 +17,12 @@ import com.mysterycoder456.standsplugin.listeners.utils.Utils;
 
 public class StandUserListener implements Listener {
 	
-	@SuppressWarnings("unused")
 	private Main plugin;
+	private FileConfiguration config;
 	
 	public StandUserListener(Main plugin) {
 		this.plugin = plugin;
+		config = plugin.getConfig();
 		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
 	
@@ -34,25 +36,36 @@ public class StandUserListener implements Listener {
 //		Star Platinum
 		if (itemInPlayerHand.getType() == Material.IRON_INGOT) {
 			double totalDamage = 16;
-//			int damageCount = 6;
-			double knockbackMultiplier = 2;
+			int hitCount = 4;
+			long attackDuration = 25;
+			double knockbackMultiplier = Double.parseDouble(config.getString("knockbackMultiplier"));
 			
 			Entity rightClickedEntity = e.getRightClicked();
 			
 			Vector knockbackVector = player.getLocation().getDirection().normalize();
-			knockbackVector.setX(knockbackVector.getX() * knockbackMultiplier);
-			knockbackVector.setY(knockbackVector.getY() * knockbackMultiplier);
-			knockbackVector.setZ(knockbackVector.getZ() * knockbackMultiplier);
+			knockbackVector.setX((knockbackVector.getX() * knockbackMultiplier) / hitCount);
+			knockbackVector.setY((knockbackVector.getY() * knockbackMultiplier + 1) / hitCount);
+			knockbackVector.setZ((knockbackVector.getZ() * knockbackMultiplier) / hitCount);
+			
+			Bukkit.broadcastMessage(Utils.chat("&a" + player.getName() + "'s Star Platinum: &7ORA ORA ORA ORA ORA!"));
 			
 //			Apply damage and knockback
-			Bukkit.broadcastMessage(Utils.chat("&a" + player.getName() + "'s Star Platinum: &7ORA ORA ORA ORA ORA!"));
-			((Damageable) rightClickedEntity).damage(totalDamage);
-			rightClickedEntity.setVelocity(knockbackVector);
+			int id = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+
+				@Override
+				public void run() {
+					((Damageable) rightClickedEntity).damage(totalDamage / hitCount);
+					rightClickedEntity.setVelocity(knockbackVector);
+				}
+			}, 0, attackDuration / hitCount);
 			
-//			TODO: Add repeated damage
-//			for (int i = 0; i < damageCount; i++) {
-//				((Damageable) rightClickedEntity).damage(totalDamage / damageCount);
-//			}
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+
+				@Override
+				public void run() {
+					Bukkit.getScheduler().cancelTask(id);
+				}
+			}, attackDuration);
 		}
 	}
 
