427c844b1b6ae270d0a5268bf0039f04d612af30
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-11-13 01:54:06 +0400

    added some sort of  Za Warudo time stop

--- src/com/mysterycoder456/standsplugin/listeners/StandUserListener.java
+++ src/com/mysterycoder456/standsplugin/listeners/StandUserListener.java
@@ -3,11 +3,13 @@ package com.mysterycoder456.standsplugin.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Fireball;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,6 +17,8 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 import com.mysterycoder456.standsplugin.Main;
@@ -107,6 +111,54 @@ public class StandUserListener implements Listener {
 	}
 	
 	
+	private void zaWarudo(Player player) {
+		
+		int timeStopDuration = Integer.parseInt(config.getString("ZW_timeStopDuration")) * 20; // times 20 to convert from seconds to ticks
+		World world = player.getWorld();
+		
+		PotionEffect slowness = new PotionEffect(PotionEffectType.SLOW, timeStopDuration, 2147483646);
+		PotionEffect slowDigging = new PotionEffect(PotionEffectType.SLOW_DIGGING, timeStopDuration, 2147483647);
+		PotionEffect slowFalling = new PotionEffect(PotionEffectType.SLOW_FALLING, timeStopDuration, 2147483647);
+		PotionEffect weekness = new PotionEffect(PotionEffectType.WEAKNESS, timeStopDuration, 2147483647);
+		
+		Bukkit.broadcastMessage(Utils.chat("&a" + player.getDisplayName() + ": &7ZA WARUDO!"));
+		Bukkit.broadcastMessage(Utils.chat("&7Time has been stopped!"));
+		
+		for (Entity entity : world.getEntities()) {
+			
+			if (!(entity instanceof LivingEntity)) {
+				continue;
+			}
+			
+			if (entity instanceof Player) {
+				
+				if (player.equals((Player) entity)) {
+					continue;
+				}
+				
+			}
+			
+			LivingEntity livingEntity = (LivingEntity) entity;
+			
+			livingEntity.addPotionEffect(slowness);
+			livingEntity.addPotionEffect(slowDigging);
+			livingEntity.addPotionEffect(slowFalling);
+			livingEntity.addPotionEffect(weekness);
+			
+		}
+				
+		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+
+			@Override
+			public void run() {
+				Bukkit.broadcastMessage(Utils.chat("&7Time has started again!"));
+			}
+			
+		}, timeStopDuration);
+		
+	}
+	
+	
 //	Stand Ability listener on Entities
 	@EventHandler
 	public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent e) {
@@ -140,6 +192,12 @@ public class StandUserListener implements Listener {
 //			Magician's Red
 			if (itemInPlayerHand.getType() == Material.BLAZE_ROD) {
 				magiciansRed(player);
+				itemInPlayerHand.setAmount(itemInPlayerHand.getAmount() - 1);
+			}
+			
+//			Za Warudo
+			else if (itemInPlayerHand.getType() == Material.NETHERITE_INGOT) {
+				zaWarudo(player);
 				itemInPlayerHand.setAmount(itemInPlayerHand.getAmount() - 1);
 			}
 		
